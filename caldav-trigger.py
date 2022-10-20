1f9af03638f286e7a1b0cce2e4fc01c803ebb4e1
Tilman Vogel <tilman.vogel@web.de>
2022-10-20 23:24:20 +0200

    Implement support for cooloff >= preheat
    Also, cleanup unused variable

--- caldav-trigger.py
+++ caldav-trigger.py
@@ -29,17 +29,20 @@ webhooks_heat_on_action = os.getenv('webhooks_heat_on_action')
 webhooks_heat_off_action = os.getenv('webhooks_heat_off_action')
 
 # main()
-
-if cooloff_minutes >= preheat_minutes:
-    print("Currently not supported: cooloff_minutes(%i) >= preheat_minutes(%i)" % (cooloff_minutes, preheat_minutes), file=sys.stderr)
-    exit(1)
-
 wrapper = textwrap.TextWrapper(initial_indent=' ' * 4, width=80, subsequent_indent=' ' * 8)
 
 now = datetime.datetime.now().astimezone()
 print("Checking at %s" % now)
-begin_search_window = now + datetime.timedelta(minutes=cooloff_minutes)
-end_search_window = now + datetime.timedelta(minutes=preheat_minutes)
+cooloff_timestamp = now + datetime.timedelta(minutes=cooloff_minutes,seconds=1)
+preheat_timestamp = now + datetime.timedelta(minutes=preheat_minutes,seconds=1)
+
+begin_search_window = now
+end_search_window = preheat_timestamp
+
+if cooloff_minutes < preheat_minutes:
+    # can use shortcut to only search from cooloff_timestamp
+    begin_search_window = cooloff_timestamp
+    cooloff_timestamp = None
 
 any_needs_heating = False
 with caldav.DAVClient(url=caldav_url, username=caldav_user, password=caldav_password) as client:
@@ -48,7 +51,6 @@ with caldav.DAVClient(url=caldav_url, username=caldav_user, password=caldav_pass
     for event in calendar.date_search(start=begin_search_window, end=end_search_window, verify_expand=True):
         vobj = event.vobject_instance
         # vobj.prettyPrint()
-        no_heat = False
         try:
             # print("Description: ", vobj.vevent.description.value)
             if vobj.vevent.description.value.find(no_heat_tag) >= 0:
@@ -58,12 +60,18 @@ with caldav.DAVClient(url=caldav_url, username=caldav_user, password=caldav_pass
         except:
             # no description in event: fine!
             pass
+        if cooloff_timestamp is not None and vobj.vevent.dtend.value <= cooloff_timestamp:
+            # cooloff already begun:
+            continue
+
         print(wrapper.fill("Found event that needs heating: %s" % vobj.vevent.summary.value))
         any_needs_heating = True
         break
 
 print(wrapper.fill("Heating needed" if any_needs_heating else "No heating needed"))
-webhooks_action_url = webhooks_url % (webhooks_heat_on_action if any_needs_heating else webhooks_heat_off_action, webhooks_key)
-request = requests.get(webhooks_action_url)
-print(wrapper.fill("Webhook request result: %i" % request.status_code))
+
+if webhooks_url:
+    webhooks_action_url = webhooks_url % (webhooks_heat_on_action if any_needs_heating else webhooks_heat_off_action, webhooks_key)
+    request = requests.get(webhooks_action_url)
+    print(wrapper.fill("Webhook request result: %i" % request.status_code))
 
